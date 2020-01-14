vladimiroltean/openil

commit c460e4622f522c9dfe0401e83024e72509ee7a7e

Move OpenIL packages back to package/
This is a squashed revert of the following commits:

54254c721310 ("openil-package: Delete Netopeer and TransAPI modules")
a5dc033ac0ad ("openil-package: Replace all open-coded uses of "package/"")
2ec6b4642eda ("Move OpenIL packages to separate folder")

Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

package/libopen62541/0001-CMakeLists.patch
@@ -1,0 +1,16 @@
+Reported here:
+
+  https://github.com/open62541/open62541/issues/1265
+
+diff -u -r a/CMakeLists.txt b/CMakeLists.txt
+--- a/CMakeLists.txt
++++ b/CMakeLists.txt
+@@ -136,7 +136,7 @@
+     # Strip release builds
+     if(CMAKE_BUILD_TYPE STREQUAL "MinSizeRel" OR CMAKE_BUILD_TYPE STREQUAL "Release")
+         add_definitions(-ffunction-sections -fdata-sections -fno-stack-protector -fno-unwind-tables
+-                        -fno-asynchronous-unwind-tables -fno-math-errno -fmerge-all-constants -fno-ident)
++                        -fno-asynchronous-unwind-tables -fno-math-errno -fno-ident)
+         set(CMAKE_C_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} -s")
+         set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -s")
+         if(APPLE)

Config.in
@@ -878,15 +878,11 @@ source "linux/Config.in"
 
 source "package/Config.in"
 
-source "openil-package/Config.in"
-
 source "fs/Config.in"
 
 source "boot/Config.in"
 
 source "package/Config.in.host"
-
-source "openil-package/Config.in.host"
 
 source "Config.in.legacy"
 

openil-package/Config.in
@@ -1,46 +1,0 @@
-menu "OpenIL additions"
-
-menu "Networking tools"
-	source "openil-package/iproute2-next/Config.in"
-endmenu
-menu "Edge-scale service"
-	source "openil-package/qoriq-edgescale-eds/Config.in"
-	source "openil-package/qoriq-eds-kubelet/Config.in"
-	source "openil-package/qoriq-eds-bootstrap/Config.in"
-endmenu
-menu "Netconf Supporting"
-	source "openil-package/pyang/Config.in"
-	source "openil-package/yang-model/Config.in"
-endmenu
-menu "Time-Sensitive Networking tools"
-	source "openil-package/tsntool/Config.in"
-endmenu
-menu "OPC UA tools"
-	source "openil-package/ua-nodeset/Config.in"
-	source "openil-package/libopen62541/Config.in"
-	source "openil-package/opc-motor-status/Config.in"
-endmenu
-menu "Fman ucode"
-	source "openil-package/fmucode/Config.in"
-endmenu
-menu "rcw image"
-	source "openil-package/openil-rcw/Config.in"
-endmenu
-menu "ppfe firmware"
-	source "openil-package/ppfe/Config.in"
-endmenu
-menu "ppa firmware"
-	source "openil-package/ppa/Config.in"
-endmenu
-menu "display port firmware"
-	source "openil-package/dp/Config.in"
-endmenu
-menu "Baremetal"
-	source "openil-package/bm-uboot/Config.in"
-	source "openil-package/icc/Config.in"
-endmenu
-menu "felixacl"
-	source "openil-package/felixacl/Config.in"
-endmenu
-
-endmenu

openil-package/Config.in.host
@@ -1,5 +1,0 @@
-menu "OpenIL host utilities"
-
-	source "openil-package/cst/Config.in.host"
-
-endmenu

openil-package/aliyun/Config.in
@@ -1,5 +1,0 @@
-config BR2_PACKAGE_ALIYUN
-	bool "Aliyun IoT SDK"
-	help
-	AliCloud's IoT Connectivity Devkit Running
-	on Embedded Devices.

commit 67fd0016a0283b81d85f6b9b22cdcf2a0a2ca969

Completely eliminate the openil-package directory
These were missed during the revert commit, since they were added
afterwards.

Fixes: c460e4622f52 ("Move OpenIL packages back to package/")
Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

openil-package/openil-rcw/0001-ls1028ardb-Enable-IIC5_PMUX-for-GPIO-function.patch.conditional
@@ -1,43 +1,0 @@
-From 33b403cee1fa215927bc65dc14baf35a2fdbe7b3 Mon Sep 17 00:00:00 2001
-From: Jerry Huang <jerry.huang@nxp.com>
-Date: Thu, 19 Sep 2019 12:12:20 +0800
-Subject: [PATCH 1/2] ls1028ardb: Enable IIC5_PMUX for GPIO function
-
-Fix the bug that NFC cann't work.
-
-Signed-off-by: Jianchao Wang <jianchao.wang@nxp.com>
-Signed-off-by: Jerry Huang <jerry.huang@nxp.com>
----
- ls1028ardb/R_SQPP_0x85bb/rcw_1300_emmcboot.rcw | 2 +-
- ls1028ardb/R_SQPP_0x85bb/rcw_1300_sdboot.rcw   | 2 +-
- 2 files changed, 2 insertions(+), 2 deletions(-)
-
-diff --git a/ls1028ardb/R_SQPP_0x85bb/rcw_1300_emmcboot.rcw b/ls1028ardb/R_SQPP_0x85bb/rcw_1300_emmcboot.rcw
-index b46ea2e..5466a63 100644
---- a/ls1028ardb/R_SQPP_0x85bb/rcw_1300_emmcboot.rcw
-+++ b/ls1028ardb/R_SQPP_0x85bb/rcw_1300_emmcboot.rcw
-@@ -26,7 +26,7 @@ SYSCLK_FREQ=600
- IIC2_PMUX=6
- IIC3_PMUX=2
- IIC4_PMUX=2
--IIC5_PMUX=2
-+IIC5_PMUX=1
- IIC6_PMUX=2
- CLK_OUT_PMUX=2
- EC1_SAI4_5_PMUX=5
-diff --git a/ls1028ardb/R_SQPP_0x85bb/rcw_1300_sdboot.rcw b/ls1028ardb/R_SQPP_0x85bb/rcw_1300_sdboot.rcw
-index 04e2208..1908d8c 100644
---- a/ls1028ardb/R_SQPP_0x85bb/rcw_1300_sdboot.rcw
-+++ b/ls1028ardb/R_SQPP_0x85bb/rcw_1300_sdboot.rcw
-@@ -26,7 +26,7 @@ SYSCLK_FREQ=600
- IIC2_PMUX=6
- IIC3_PMUX=2
- IIC4_PMUX=2
--IIC5_PMUX=2
-+IIC5_PMUX=1
- IIC6_PMUX=3
- CLK_OUT_PMUX=2
- EC1_SAI4_5_PMUX=5
--- 
-2.17.1
-

openil-package/openil-rcw/0002-ls1028ardb-Enable-CLK_OUT_PMUX-for-GPIO-function.patch.conditional
@@ -1,40 +1,0 @@
-From 4a3c9ea5d0c7bf8eec8cb6276d6f4d940f3946db Mon Sep 17 00:00:00 2001
-From: Jerry Huang <jerry.huang@nxp.com>
-Date: Thu, 19 Sep 2019 12:13:48 +0800
-Subject: [PATCH 2/2] ls1028ardb/rcw: Enable CLK_OUT_PMUX for GPIO function
-
-Signed-off-by: Jianchao Wang <jianchao.wang@nxp.com>
-Signed-off-by: Jerry Huang <jerry.huang@nxp.com>
----
- ls1028ardb/R_SQPP_0x85bb/rcw_1300_emmcboot.rcw | 2 +-
- ls1028ardb/R_SQPP_0x85bb/rcw_1300_sdboot.rcw   | 2 +-
- 2 files changed, 2 insertions(+), 2 deletions(-)
-
-diff --git a/ls1028ardb/R_SQPP_0x85bb/rcw_1300_emmcboot.rcw b/ls1028ardb/R_SQPP_0x85bb/rcw_1300_emmcboot.rcw
-index 5466a63..a5ed4d4 100644
---- a/ls1028ardb/R_SQPP_0x85bb/rcw_1300_emmcboot.rcw
-+++ b/ls1028ardb/R_SQPP_0x85bb/rcw_1300_emmcboot.rcw
-@@ -28,7 +28,7 @@ IIC3_PMUX=2
- IIC4_PMUX=2
- IIC5_PMUX=1
- IIC6_PMUX=2
--CLK_OUT_PMUX=2
-+CLK_OUT_PMUX=1
- EC1_SAI4_5_PMUX=5
- EC1_SAI3_6_PMUX=5
- USB3_CLK_FSEL=39
-diff --git a/ls1028ardb/R_SQPP_0x85bb/rcw_1300_sdboot.rcw b/ls1028ardb/R_SQPP_0x85bb/rcw_1300_sdboot.rcw
-index 1908d8c..c930b83 100644
---- a/ls1028ardb/R_SQPP_0x85bb/rcw_1300_sdboot.rcw
-+++ b/ls1028ardb/R_SQPP_0x85bb/rcw_1300_sdboot.rcw
-@@ -28,7 +28,7 @@ IIC3_PMUX=2
- IIC4_PMUX=2
- IIC5_PMUX=1
- IIC6_PMUX=3
--CLK_OUT_PMUX=2
-+CLK_OUT_PMUX=1
- EC1_SAI4_5_PMUX=5
- EC1_SAI3_6_PMUX=5
- USB3_CLK_FSEL=39
--- 
-2.17.1

openil-package/openil-rcw/Config.in
@@ -1,9 +1,0 @@
-config BR2_PACKAGE_OPENIL_RCW
-	bool "rcw image"
-	help
-	The reset configuration word for NXP Layerscape platforms
-
-if BR2_PACKAGE_OPENIL_RCW
-config BR2_PACKAGE_OPENIL_RCW_BIN
-	string "Custom RCW"
-endif

openil-package/openil-rcw/Config.in.host
@@ -1,12 +1,0 @@
-config BR2_PACKAGE_HOST_OPENIL_RCW
-	bool "host rcw"
-	help
-	  This package provides an reset configuration word(RCW)
-	  compiler to build NXP QoriQ/LS PBL/RCW binary(s).
-
-	  The package installs example configurations and scripts
-	  for each devkit supported in the current release see
-	  $(HOST_DIR)/share/rcw. Either a SDK or post scripts can
-	  then use this toolset and examples.
-
-	  https://source.codeaurora.org/external/qoriq/qoriq-components/rcw/

openil-package/openil-rcw/openil-rcw.mk
@@ -1,36 +1,0 @@
-################################################################################
-#
-# openil-rcw image
-#
-################################################################################
-
-OPENIL_RCW_VERSION = ls1028a-early-access
-OPENIL_RCW_SITE = https://source.codeaurora.org/external/qoriq/qoriq-components/rcw
-OPENIL_RCW_SITE_METHOD = git
-OPENIL_RCW_LICENSE = BSD License
-OPENIL_RCW_LICENSE_FILES = LICENSE
-
-OPENIL_RCW_BIN = $(call qstrip,$(BR2_PACKAGE_OPENIL_RCW_BIN))
-OPENIL_RCW_PLATFORM = $(firstword $(subst /, ,$(OPENIL_RCW_BIN)))
-
-FIND = ls1028ardb
-ifeq ($(findstring $(FIND), $(BR2_TARGET_UBOOT_BOARDNAME)), $(FIND))
-define OPENIL_RCW_CONFIGURE_CMDS
-	$(APPLY_PATCHES) $(@D) $(OPENIL_RCW_PKGDIR) \
-		0001-ls1028ardb-Enable-IIC5_PMUX-for-GPIO-function.patch.conditional; \
-	$(APPLY_PATCHES) $(@D) $(OPENIL_RCW_PKGDIR) \
-		0002-ls1028ardb-Enable-CLK_OUT_PMUX-for-GPIO-function.patch.conditional;
-endef
-endif
-
-define OPENIL_RCW_BUILD_CMDS
-	cd $(@D)/$(OPENIL_RCW_PLATFORM) && $(MAKE); \
-	if [ $(OPENIL_RCW_PLATFORM) = ls1046ardb ] || [ $(OPENIL_RCW_PLATFORM) = ls1012ardb ]; then \
-		tclsh board/nxp/common/byte_swap.tcl $(@D)/$(OPENIL_RCW_BIN) $(@D)/$(OPENIL_RCW_BIN) 8; \
-		cp -f $(@D)/$(OPENIL_RCW_BIN).swapped $(BINARIES_DIR); \
-	else \
-		cp -f $(@D)/$(OPENIL_RCW_BIN) $(BINARIES_DIR); \
-	fi
-endef
-
-$(eval $(generic-package))

package/Config.in
@@ -2014,6 +2014,7 @@ menu "Networking applications"
 	source "package/iperf/Config.in"
 	source "package/iperf3/Config.in"
 	source "package/iproute2/Config.in"
+	source "package/iproute2-next/Config.in"
 	source "package/ipsec-tools/Config.in"
 	source "package/ipset/Config.in"
 	source "package/iptables/Config.in"
@@ -2382,6 +2383,7 @@ endmenu
 menu "OPC UA tools"
 	source "package/libopen62541/Config.in"
 	source "package/opc-motor-status/Config.in"
+	source "package/ua-nodeset/Config.in"
 endmenu
 menu "Fman ucode"
 	source "package/fmucode/Config.in"

package/libopen62541/0001-CMakeLists.patch
@@ -1,16 +1,0 @@
-Reported here:
-
-  https://github.com/open62541/open62541/issues/1265
-
-diff -u -r a/CMakeLists.txt b/CMakeLists.txt
---- a/CMakeLists.txt
-+++ b/CMakeLists.txt
-@@ -136,7 +136,7 @@
-     # Strip release builds
-     if(CMAKE_BUILD_TYPE STREQUAL "MinSizeRel" OR CMAKE_BUILD_TYPE STREQUAL "Release")
-         add_definitions(-ffunction-sections -fdata-sections -fno-stack-protector -fno-unwind-tables
--                        -fno-asynchronous-unwind-tables -fno-math-errno -fmerge-all-constants -fno-ident)
-+                        -fno-asynchronous-unwind-tables -fno-math-errno -fno-ident)
-         set(CMAKE_C_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} -s")
-         set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -s")
-         if(APPLE)