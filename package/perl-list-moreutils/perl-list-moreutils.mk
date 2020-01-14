c0eb83ed274d294409d5fa156691bd2ee4666836
Vladimir Oltean <vladimir.oltean@nxp.com>
2020-01-14 19:13:34 +0200

    Re-surface programs for LS1021A-TSN demo
    The feedgnuplot, nodejs-lbt and prl programs are needed for a web
    application that demonstrates bandwidth vs latency on the NXP
    LS1021A-TSN board. These packages were lost during various merge
    processes. Bring them back to life, and do some small cleanups at the
    same time.
    
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

--- package/perl-list-moreutils/perl-list-moreutils.mk
+++ package/perl-list-moreutils/perl-list-moreutils.mk
@@ -1,7 +1,14 @@
-PERL_LIST_MOREUTILS_VERSION = 0.426
+################################################################################
+#
+# perl-list-moreutils
+#
+################################################################################
+
+PERL_LIST_MOREUTILS_VERSION = 0.428
 PERL_LIST_MOREUTILS_SOURCE = List-MoreUtils-$(PERL_LIST_MOREUTILS_VERSION).tar.gz
 PERL_LIST_MOREUTILS_SITE = http://search.cpan.org/CPAN/authors/id/R/RE/REHSACK
 PERL_LIST_MOREUTILS_LICENSE = Apache
 PERL_LIST_MOREUTILS_LICENSE_FILES = LICENSE
+PERL_LIST_MOREUTILS_DISTNAME = perl-list-moreutils
 
 $(eval $(perl-package))
