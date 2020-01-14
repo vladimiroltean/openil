74bd875473e4c77f567cc8e6df597b9ccdf4080e
Vladimir Oltean <vladimir.oltean@nxp.com>
2020-01-14 19:11:51 +0200

    Get rid of conditional patches
    The LS1028A-RDB needs some custom RCW changes. Move those to a global
    patch directory for this board instead of polluting the RCW makefile.
    
    The transapi module appears to not be using conditional patches for any
    reason. Rename them to .patch and remove the manual application from the
    makefile.
    
    The kernel patches for ls1028a-rdb had to be moved from board/nxp/common
    to board/nxp/ls1028ardb because we can't have 2 BR2_GLOBAL_PATCH_DIR
    variables, and because the conditional patches for RCW need to be
    applied only for LS1028A-RDB.
    
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

--- package/rcw/rcw.mk
+++ package/rcw/rcw.mk
@@ -13,15 +13,6 @@ RCW_LICENSE_FILES = LICENSE
 RCW_BIN = $(call qstrip,$(BR2_PACKAGE_RCW_BIN))
 RCW_PLATFORM = $(firstword $(subst /, ,$(RCW_BIN)))
 
-FIND = ls1028ardb
-ifeq ($(findstring $(FIND), $(BR2_TARGET_UBOOT_BOARDNAME)), $(FIND))
-define RCW_CONFIGURE_CMDS
-	patch -p1 -s -d $(@D) < $(TARGET_DIR)/../../package/rcw/0001-ls1028ardb-Enable-IIC5_PMUX-for-GPIO-function.patch.conditional;\
-	patch -p1 -s -d $(@D) < $(TARGET_DIR)/../../package/rcw/0002-ls1028ardb-Enable-CLK_OUT_PMUX-for-GPIO-function.patch.conditional;\
-	patch -p1 -s -d $(@D) < $(TARGET_DIR)/../../package/rcw/0003-Enable-SAI-for-LS1028ARDB-baremetal.patch.conditional
-endef
-endif
-
 define RCW_BUILD_CMDS
 	cd $(@D)/$(RCW_PLATFORM) && $(MAKE); \
 	cp -f $(@D)/$(RCW_BIN) $(BINARIES_DIR);
